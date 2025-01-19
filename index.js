4e936c84e6ad1d791c7abf093ac71dbc1fabd888
mdimteyazhossen <imteyazhossen13711@gmail.com>
2025-01-19 19:20:42 +0600

    create a route for all medicine data using get method

--- index.js
+++ index.js
@@ -25,12 +25,21 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const medicine = client.db("MediBuyersDB").collection("meidicine")
+
+        app.get('/medicine', async(req,res)=>{
+            const result = await medicine.find().toArray();
+            // console.log(result)
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
